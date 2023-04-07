c598895882d87eb356d7a4bc1210e2c1443996ca
michaelgiba <michaelgiba@gmail.com>
2023-04-07 16:47:54 -0500

    Continued testing

--- examples/model-io/src/main.rs
+++ examples/model-io/src/main.rs
@@ -7,22 +7,27 @@ mod tests {
     use ggml_rs::Context;
     use ggml_rs::Dimension;
     use std::fs::File;
+    use std::io::Seek;
 
-    #[model_io]
-    struct FourU8Parameter {
-        k: [u8; 4],
+    #[model_io(ggml_datatype = i8)]
+    struct EightBitParam {
+        k: i8,
     }
 
-    #[model_io]
+    #[model_io(ggml_datatype = i32)]
+    struct SixteenBitParam {
+        k: i32,
+    }
+
+    #[model_io(ggml_datatype = i8)]
     struct RectU8Param {
-        k: [[u8; 4]; 8],
+        k: [[i8; 4]; 8],
     }
 
-    #[model_io]
+    #[model_io(ggml_datatype = i8)]
     struct CubeU8Param {
-        k: [[[u8; 2]; 2]; 2],
+        k: [[[i8; 2]; 2]; 2],
     }
-
 
     const MEMORY_SIZE: usize = 1024 * 512;
 
@@ -43,18 +48,41 @@ mod tests {
         let test_file_path = test_file!("resources/model.bin");
         let mut reader = File::open(&test_file_path).expect("Failed to open file");
 
-        for _ in 0..4 {
+        for _ in 0..16 {
             let read_result =
-                FourU8Parameter::read_to_tensor(&ctx, &mut reader, Dimension::D1, vec![None]);
+                EightBitParam::read_to_tensor(&ctx, &mut reader, Dimension::D1, vec![None]);
             assert!(read_result.is_ok());
             let tensor = read_result.unwrap();
-            assert_eq!(tensor.nbytes(), 4);
+            assert_eq!(tensor.nbytes(), 1);
         }
         assert!(
-            FourU8Parameter::read_to_tensor(&ctx, &mut reader, Dimension::D1, vec!(None)).is_err()
+            EightBitParam::read_to_tensor(&ctx, &mut reader, Dimension::D1, vec!(None)).is_err()
         );
     }
 
+    #[test]
+    fn test_reader_1d_2() {
+        let mut buffer: ManagedMemory = ManagedMemory([0; MEMORY_SIZE]);
+        let ctx = Context::init_managed(&mut buffer.0);
+
+        let test_file_path = test_file!("resources/model.bin");
+        let mut reader = File::open(&test_file_path).expect("Failed to open file");
+
+        for _ in 0..16 {
+            let read_result =
+                SixteenBitParam::read_to_tensor(&ctx, &mut reader, Dimension::D1, vec![Some(1)]);
+            println!("{:?}", reader.stream_position());
+
+            assert!(read_result.is_ok());
+            let tensor = read_result.unwrap();
+            println!("{:?}", tensor.read_elements::<u8>(0, 4));
+
+            assert_eq!(tensor.nbytes(), 4);
+        }
+        assert!(
+            SixteenBitParam::read_to_tensor(&ctx, &mut reader, Dimension::D1, vec!(None)).is_err()
+        );
+    }
 
     #[test]
     fn test_reader_2d() {
@@ -65,16 +93,21 @@ mod tests {
         let mut reader = File::open(&test_file_path).expect("Failed to open file");
 
         for _ in 0..2 {
-            let read_result =
-                RectU8Param::read_to_tensor(&ctx, &mut reader, Dimension::D2, vec![Some(16), Some(2)]);
+            let read_result = RectU8Param::read_to_tensor(
+                &ctx,
+                &mut reader,
+                Dimension::D2,
+                vec![Some(16), Some(2)],
+            );
             assert!(read_result.is_ok());
             let tensor = read_result.unwrap();
             assert_eq!(tensor.nbytes(), 32);
         }
         assert!(
-            RectU8Param::read_to_tensor(&ctx, &mut reader, Dimension::D2, vec!(None, None)).is_err()
+            RectU8Param::read_to_tensor(&ctx, &mut reader, Dimension::D2, vec!(None, None))
+                .is_err()
         );
-    }    
+    }
 
     #[test]
     fn test_reader_3d() {
@@ -85,15 +118,22 @@ mod tests {
         let mut reader = File::open(&test_file_path).expect("Failed to open file");
 
         for _ in 0..8 {
-            let read_result =
-                CubeU8Param::read_to_tensor(&ctx, &mut reader, Dimension::D3, vec![Some(2), Some(2), Some(2)]);
+            let read_result = CubeU8Param::read_to_tensor(
+                &ctx,
+                &mut reader,
+                Dimension::D3,
+                vec![Some(2), Some(2), Some(2)],
+            );
             assert!(read_result.is_ok());
             let tensor = read_result.unwrap();
             assert_eq!(tensor.nbytes(), 8);
         }
-        assert!(
-            CubeU8Param::read_to_tensor(&ctx, &mut reader, Dimension::D3, vec!(None, None, None)).is_err()
-        );
-    }    
-
+        assert!(CubeU8Param::read_to_tensor(
+            &ctx,
+            &mut reader,
+            Dimension::D3,
+            vec!(None, None, None)
+        )
+        .is_err());
+    }
 }
--- ggml-rs/ggml-io/src/lib.rs
+++ ggml-rs/ggml-io/src/lib.rs
@@ -2,18 +2,42 @@
 
 extern crate proc_macro;
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use quote::{quote, ToTokens};
+use syn::Lit::Str;
+use syn::{parse_macro_input, DeriveInput, LitStr};
 
-#[proc_macro_derive(ModelIO, attributes(dimension))]
+#[proc_macro_derive(ModelIO, attributes(tensor_params))]
 pub fn derive_model_io(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
+    let attr: Vec<_> = input
+        .attrs
+        .iter()
+        .filter(|attr| attr.path.is_ident("tensor_params"))
+        .collect();
+
+    let datatype_name_value: syn::MetaNameValue = attr[0].parse_args().unwrap();
+
+    let tensor_type_string = match datatype_name_value.lit {
+        Str(string_val) => string_val.value(),
+        _ => panic!("Unknown value provided to datatype in macro."),
+    };
+
+    let ggml_dtype = match tensor_type_string.as_str() {
+        "i8" => quote! { ggml_rs::DataType::I8 },
+        "i16" => quote! { ggml_rs::DataType::I16 },
+        "i32" => quote! { ggml_rs::DataType::I32 },
+        "f16" => quote! { ggml_rs::DataType::F16 },
+        "f32" => quote! { ggml_rs::DataType::F32 },
+        "count" => quote! { ggml_rs::DataType::COUNT },
+        _ => panic!("Invalid datatype provided."),
+    };
+
     let gen = quote! {
 
         macro_rules! model_io_bincode_config {
-            () => { bincode::config::standard().skip_fixed_array_length() };
+            () => { bincode::config::standard().skip_fixed_array_length().with_variable_int_encoding() };
         }
 
         impl ggml_rs::io::ModelIO for #name {
@@ -24,7 +48,10 @@ pub fn derive_model_io(input: TokenStream) -> TokenStream {
                 shape: Vec<Option<usize>>
             ) -> Result<ggml_rs::Tensor, ()> {
                 match Self::read(ctx, reader) {
-                    Ok(serialized) => serialized.to_tensor(ctx, dim, shape),
+                    Ok(serialized) => {
+                        println!("{:?}", serialized);
+                        serialized.to_tensor(ctx, dim, shape)
+                    },
                     Err(_) => {
                         Err(())
                     }
@@ -43,16 +70,16 @@ pub fn derive_model_io(input: TokenStream) -> TokenStream {
                 let new_tensor = match dim {
                     ggml_rs::Dimension::Scalar => ctx.new_f32(0.0),
                     ggml_rs::Dimension::D1 => ctx.new_tensor_1d(
-                        ggml_rs::DataType::I8,
+                        #ggml_dtype,
                         shape[0].unwrap_or(buf.len())
                     ),
                     ggml_rs::Dimension::D2 => ctx.new_tensor_2d(
-                        ggml_rs::DataType::I8,
+                        #ggml_dtype,
                         shape[0].unwrap_or(buf.len()),
                         shape[1].unwrap_or(1), // WRONG - properly infer shapes
                     ),
                     ggml_rs::Dimension::D3 => ctx.new_tensor_3d(
-                        ggml_rs::DataType::I8,
+                        #ggml_dtype,
                         shape[0].unwrap_or(buf.len()),
                         shape[1].unwrap_or(1), // WRONG - properly infer shapes
                         shape[2].unwrap_or(1), // WRONG - properly infer shapes
@@ -72,9 +99,7 @@ pub fn derive_model_io(input: TokenStream) -> TokenStream {
                 reader: &mut R
             ) -> Result<Self, bincode::error::DecodeError> {
                 let config = model_io_bincode_config!();
-                bincode::decode_from_std_read(
-                    reader, config
-                )
+                bincode::decode_from_std_read(reader, config)
             }
 
             fn write(&self, path: &str) -> Result<(), ggml_rs::io::ModelIOError> {
@@ -87,12 +112,27 @@ pub fn derive_model_io(input: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn model_io(
-    _metadata: proc_macro::TokenStream,
+    metadata: proc_macro::TokenStream,
     input: proc_macro::TokenStream,
 ) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
+
+    let attr_terms: Vec<String> = metadata.into_iter().map(|x| x.to_string()).collect();
+    let datatype: String = match attr_terms.len() {
+        0 => String::from("i8"),
+        3 => {
+            if attr_terms[0] == "ggml_datatype" && attr_terms[1] == "=" {
+                String::from(attr_terms[2].clone())
+            } else {
+                panic!("Invalid attribute provided to macro.")
+            }
+        }
+        _ => panic!("Invalid attribute provided to macro."),
+    };
+
     let output = quote! {
         #[derive(Debug, bincode::Decode, bincode::Encode, ggml_rs::io::ModelIO)]
+        #[tensor_params(datatype=#datatype)]
         #input
     };
     output.into()
